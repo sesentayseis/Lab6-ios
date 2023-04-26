4bb7d2699625d354b7611580f41a4d5cabe90289
Cinthia Calla Ayma <cinthia.calla@tecsup.edu.pe>
2023-04-26 10:47:32 -0500

    Initial Commit

---
+++ CondoriTableViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CondoriTableViewController
+//
+//  Created by Mac 05 on 26/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
